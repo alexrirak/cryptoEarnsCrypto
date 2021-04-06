a0103432efd8a1de02e75a724dc4bd5ceed64b6f
Alex Rirak <alexrirak@gmail.com>
2021-04-05 23:54:18 -0400

    fixing update batch + models

--- app/Models/CoinMetadata.php
+++ app/Models/CoinMetadata.php
@@ -20,16 +20,4 @@ class CoinMetadata extends Model
      * @var string
      */
     protected $keyType = 'string';
-
-    /**
-     * Overide the default boot method to create our uuid
-     */
-    protected static function boot() {
-
-        parent::boot();
-
-        static::creating(function (Model $model) {
-            $model->setAttribute($model->getKeyName(), Uuid::uuid4());
-        });
-    }
 }
