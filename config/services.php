731f9de7df2f98b73434f988f97e3497b1e7c78d
Alex Rirak <alexrirak@gmail.com>
2021-04-13 22:06:53 -0400

    facebook auth

--- config/services.php
+++ config/services.php
@@ -36,4 +36,10 @@ return [
         'redirect'      => env('GOOGLE_URL'),
     ],
 
+    'facebook' => [
+        'client_id'     => env('FACEBOOK_CLIENT_ID'),
+        'client_secret' => env('FACEBOOK_CLIENT_SECRET'),
+        'redirect'      => env('FACEBOOK_URL'),
+    ],
+
 ];
