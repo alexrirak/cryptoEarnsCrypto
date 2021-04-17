fea5b06e4d7726a9113ade1763c26a3303d19a3b
Alex Rirak <alexrirak@gmail.com>
2021-04-16 20:01:01 -0400

    navbar rewrite

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -18,47 +18,71 @@
             <div class="corner-ribbon top-left sticky orange shadow" style="z-index: 9999;">{{ Str::upper(config('app.env')) }}</div>
         @endif
 
+
         @include('cookieConsent::index')
 
-        <header class="p-2 bg-dark text-white">
-            <div class="container">
-                <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start ">
-                    <a class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none navbar-brand" href="{{ config('app.url') }}">Crypto Earns Crypto</a>
+        <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+            <div class="container-lg">
+                    <a class="navbar-brand" href="{{ config('app.url') }}">Crypto Earns Crypto</a>
 
-                    <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-{{--                        <li><a href="#" class="nav-link px-2 text-secondary">Home</a></li>--}}
-{{--                        <li><a href="#" class="nav-link px-2 text-white">Features</a></li>--}}
-{{--                        <li><a href="#" class="nav-link px-2 text-white">Pricing</a></li>--}}
-{{--                        <li><a href="#" class="nav-link px-2 text-white">FAQs</a></li>--}}
-{{--                        <li><a href="#" class="nav-link px-2 text-white">About</a></li>--}}
-                    </ul>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
 
+                    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
 
-                    <div class="text-end">
-                        @auth
-                            <div class="flex-shrink-0 dropdown">
-                                <a href="#" class="d-block link-dark text-decoration-none dropdown-toggle" id="dropdownUser2" data-bs-toggle="dropdown" aria-expanded="false">
-                                    <svg width="32" height="32">
-                                        <circle cx="16" cy="16" r="16" fill="#aeaeae" />
-                                        <text x="50%" y="50%" text-anchor="middle" fill="white" font-size="16px" font-family="Arial" dy=".4em">{{ Auth::user()->initials() }}</text>
-                                    </svg>
+                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li class="nav-item">
+                                <a class="nav-link {{ Route::is('home') ? 'active' : '' }}" aria-current="page" href="{{ route('home') }}">Home</a>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    APY Trackers
                                 </a>
-                                <ul class="dropdown-menu text-small shadow" aria-labelledby="dropdownUser2">
-                                    <li><a class="dropdown-item" href="#">New project...</a></li>
-                                    <li><a class="dropdown-item" href="#">Settings</a></li>
-                                    <li><a class="dropdown-item" href="#">Profile</a></li>
-                                    <li><hr class="dropdown-divider"></li>
-                                    <li><a class="dropdown-item" href="{{ route('logout') }}">Sign out</a></li>
+                                <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                    <li><a class="dropdown-item" href="#">Celsius</a></li>
+                                    <li><a class="dropdown-item" href="#">Another Provider</a></li>
+                                    <li><a class="dropdown-item" href="#">Something else here</a></li>
                                 </ul>
-                            </div>
-                        @else
-                            <a class="btn btn-outline-light me-2" href="{{ route('login') }}" role="button">Login</a>
-                            <a class="btn btn-warning" href="{{ route('login') }}" role="button">Sign-up</a>
-                        @endauth
+                            </li>
+                            <li><hr class="dropdown-divider"></li>
+                            @auth
+                                <li class="nav-item d-md-none">
+                                    <a class="nav-link" aria-current="page" href="{{ route('logout') }}">Sign out</a>
+                                </li>
+                            @else
+                                <li class="nav-item d-md-none">
+                                    <a class="nav-link" aria-current="page" href="{{ route('login') }}">Login / Sign-up</a>
+                                </li>
+                            @endauth
+                        </ul>
+
+                        <div class="d-flex d-none d-md-block">
+                            @auth
+                                <div class="flex-shrink-0 dropdown">
+                                    <a href="#" class="d-block link-dark text-decoration-none dropdown-toggle" id="dropdownUser2" data-bs-toggle="dropdown" aria-expanded="false">
+                                        <svg width="32" height="32">
+                                            <circle cx="16" cy="16" r="16" fill="#aeaeae" />
+                                            <text x="50%" y="50%" text-anchor="middle" fill="white" font-size="16px" font-family="Arial" dy=".4em">{{ Auth::user()->initials() }}</text>
+                                        </svg>
+                                    </a>
+                                    <ul class="dropdown-menu text-small shadow" aria-labelledby="dropdownUser2">
+                                        <li><a class="dropdown-item" href="#">New project...</a></li>
+                                        <li><a class="dropdown-item" href="#">Settings</a></li>
+                                        <li><a class="dropdown-item" href="#">Profile</a></li>
+                                        <li><hr class="dropdown-divider"></li>
+                                        <li><a class="dropdown-item" href="{{ route('logout') }}">Sign out</a></li>
+                                    </ul>
+                                </div>
+                            @else
+                                <a class="btn btn-outline-light me-2" href="{{ route('login') }}" role="button">Login</a>
+                                <a class="btn btn-warning" href="{{ route('login') }}" role="button">Sign-up</a>
+                            @endauth
+                        </div>
                     </div>
-                </div>
+
             </div>
-        </header>
+        </nav>
 
         <main class="container-lg">
 
