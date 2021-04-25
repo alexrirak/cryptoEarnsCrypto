758b83d537356d7b93ec4d093f1093177708deaa
Alex Rirak <alexrirak@gmail.com>
2021-04-25 17:51:24 -0400

    tweak

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             CoinMetadataSeeder::class,
             RateSeeder::class,
+            ProviderMetadataSeeder::class,
         ]);
     }
 }
