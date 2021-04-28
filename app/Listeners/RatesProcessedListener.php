aec5d48166ba4c50424d2e872bdf4d137c9c78c1
Alex Rirak <alexrirak@gmail.com>
2021-04-27 22:30:21 -0400

    rethinking notifications

--- app/Listeners/RatesProcessedListener.php
+++ app/Listeners/RatesProcessedListener.php
@@ -6,6 +6,9 @@ use App\Events\RatesProcessed;
 use App\Helpers\EmailHelper;
 use App\Models\EmailLog;
 use App\Models\ProviderMetadata;
+use App\Models\Rate;
+use App\Models\UserNotification;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 
 class RatesProcessedListener
@@ -31,7 +34,7 @@ class RatesProcessedListener
     /**
      * Handle the event.
      *
-     * @param  RatesProcessed  $event
+     * @param RatesProcessed $event
      * @return void
      */
     public function handle(RatesProcessed $event)
@@ -45,13 +48,31 @@ class RatesProcessedListener
             return;
         }
 
-        $emailLog = EmailLog::where('provider_id','=',$provider->id)
-                            ->where('type','=','UPDATE')
+        $emailLog = EmailLog::where('provider_id', '=', $provider->id)
+                            ->where('type', '=', 'UPDATE')
                             ->orderBy('created_at')
                             ->first();
 
-        $lastUpdate = $emailLog ? $emailLog->created_at : date("Y-m-d H:i:s");
+        $lastUpdate = $emailLog ? $emailLog->created_at : date("Y-m-d H:i:s", strtotime("-1 days"));
+        $currentDate = date("Y-m-d H:i:s");
 
-        $this->emailHelper->sendMail($lastUpdate);
+        Log::info(sprintf("[%s] Fetching rates from %s to %s", $provider->name, $lastUpdate, $currentDate));
+
+        $usersTonotify = Rate::join('user_notifications', 'rates.coin_id', '=', 'user_notifications.coin_id')
+                             ->join('users', 'user_notifications.user_id', '=', 'users.id')
+                             ->select('users.id', 'users.name', 'users.email')
+                             ->where('rates.source', '=', $provider->name)
+                             ->whereBetween('rates.created_at', [$lastUpdate, $currentDate])
+                             ->groupBy('users.id', 'users.name', 'users.email')
+                             ->get();
+
+        Log::debug(sprintf("[%s] Users needing notification: %s", $provider->name, $usersTonotify));
+
+        if (count($usersTonotify) == 0) {
+            Log::info(sprintf("[%s] No updates to send since last run", $provider->name));
+            return;
+        }
+
+        $this->emailHelper->sendMail($usersTonotify);
     }
 }
