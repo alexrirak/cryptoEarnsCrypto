564e5c754791ced47d219e4a54d8b8d182d74e91
Alex Rirak <alexrirak@gmail.com>
2021-05-25 22:07:43 -0400

    session updates

--- config/session.php
+++ config/session.php
@@ -18,7 +18,7 @@ return [
     |
     */
 
-    'driver' => env('SESSION_DRIVER', 'file'),
+    'driver' => env('SESSION_DRIVER', 'cookie'),
 
     /*
     |--------------------------------------------------------------------------
@@ -31,7 +31,7 @@ return [
     |
     */
 
-    'lifetime' => env('SESSION_LIFETIME', 120),
+    'lifetime' => env('SESSION_LIFETIME', 1440), //60 * 24 =
 
     'expire_on_close' => false,
 
