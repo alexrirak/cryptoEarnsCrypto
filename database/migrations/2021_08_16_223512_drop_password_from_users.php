d575c74d1934666c0a0d91e5ebe7ba8cc1f1ac6a
Alex Rirak <alexrirak@gmail.com>
2021-08-16 22:43:57 -0400

    Fix column with no default, drop password field

--- database/migrations/2021_08_16_223512_drop_password_from_users.php
+++ database/migrations/2021_08_16_223512_drop_password_from_users.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddAdminFlagToUsers extends Migration
+class DropPasswordFromUsers extends Migration
 {
     /**
      * Run the migrations.
@@ -14,7 +14,7 @@ class AddAdminFlagToUsers extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->boolean('isAdmin')->default(false);
+            $table->dropColumn('password');
         });
     }
 
@@ -26,7 +26,7 @@ class AddAdminFlagToUsers extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn('isAdmin');
+            $table->string('password')->nullable();
         });
     }
 }
